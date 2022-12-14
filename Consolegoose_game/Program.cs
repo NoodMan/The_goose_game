db11a21ac9baa5eecbbf8b4d32ef08265f839237
wguillaume <guillaume.weidmann@gmail.com>
2022-12-14 15:48:51 +0100

    ✨ add class IniGame

--- Consolegoose_game/Program.cs
+++ Consolegoose_game/Program.cs
@@ -1,17 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Consolegoose_game;
 
-internal class Program
+public class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
 
-        Dice dice = new Dice();
-        Console.WriteLine(dice.Throw());
+ 
+        IniGame.Initialization();
 
-        Player player = new Player("guillaume");
-        player.DiceDraw();
+
 
         
 
